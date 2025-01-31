4963c333a400b61ff2cf19ee15bb2b386e9dd1f5
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-01-31 11:34:41 -0800

    Finished Finance tracker
    Displayed results and made a conditional message based on the balance.

--- finance-tracker.py
+++ finance-tracker.py
@@ -2,8 +2,16 @@
 print("Welcome to the finance tracker")
 allowance = float(input("What is your monthly allowance?"))
 expenses = float(input("How much is your monthly spending?"))
+
 # Calculate total expenses and remaining balance
 balance = round(allowance - expenses, 2)
-# Display results
 
-# Conditional message based on balance (depending on whether you overspent or underspent or spent just the right amount, write a specific message - try to be kind!)
+""" Display results
+ Conditional message based on balance (depending on whether 
+ you overspent or underspent or spent just the right amount, 
+ write a specific message - try to be kind!)"""
+if balance < 0:
+    print(f"${balance}")
+    print("You overspent. You need to watch how much you're spending")
+else:
+    print(f"You have ${balance}")
