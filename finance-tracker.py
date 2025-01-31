e3473316d29c53311ca426bdacd4ccbc5a480a97
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-01-31 11:15:27 -0800

    Made calculations
    Made calculations an rounded balance to two decimal points.

--- finance-tracker.py
+++ finance-tracker.py
@@ -3,7 +3,7 @@ print("Welcome to the finance tracker")
 allowance = float(input("What is your monthly allowance?"))
 expenses = float(input("How much is your monthly spending?"))
 # Calculate total expenses and remaining balance
-
+balance = round(allowance - expenses, 2)
 # Display results
 
 # Conditional message based on balance (depending on whether you overspent or underspent or spent just the right amount, write a specific message - try to be kind!)
