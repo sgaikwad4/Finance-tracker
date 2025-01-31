955260f615d3992dadb0f41d76d8cd12ccc7217f
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-01-31 11:09:19 -0800

    Ask for monthly allowance and expenses
    Greeted the user and asked for their monthly earnings and expenses.

--- finance-tracker.py
+++ finance-tracker.py
@@ -1,5 +1,7 @@
 # Ask for monthly allowance and expenses in different categories
-
+print("Welcome to the finance tracker")
+allowance = float(input("What is your monthly allowance?"))
+expenses = float(input("How much is your monthly spending?"))
 # Calculate total expenses and remaining balance
 
 # Display results
